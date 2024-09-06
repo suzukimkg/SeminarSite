b4a544a12bb4072dfafccc8dee225b9ddb80d186
Arata <aratadayoon@yahoo.co.jp>
2024-09-07 07:20:36 +0900

    update:

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,11 @@ import styles from './layout.module.css';
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
-  title: 'Simple Blog',
-  description: 'A simple blog presented by microCMS',
+  title: 'ゼミ',
+  description: 'A ゼミ presented by microCMS',
   openGraph: {
-    title: 'Simple Blog',
-    description: 'A simple blog presented by microCMS',
+    title: 'ゼミ',
+    description: 'A ゼミ presented by microCMS',
     images: '/ogp.png',
   },
   alternates: {
