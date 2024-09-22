b87dfdc368501e643cc03214419827575ac45be8
Arata <aratadayoon@yahoo.co.jp>
2024-09-22 18:07:41 +0900

    #1 トップページ meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,9 @@ import { getTagList } from '@/libs/microcms';
 import { LIMIT } from '@/constants';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import Nav from '@/components/Nav';
 import './globals.css';
 import styles from './layout.module.css';
 import Script from 'next/script';
-import { url } from 'inspector';
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
@@ -15,14 +13,11 @@ export const metadata = {
   openGraph: {
     title: '鈴木ゼミ｜東洋大学経営学部マーケティング学科鈴木ゼミナール',
     description: '東洋大学経営学部マーケティング学科鈴木ゼミナールの公式ホームページです。',
-    images: '/ogp.png',
-    url: '',
-  },
-  alternates: {
-    canonical: '',
+    images: '/images/icons/1200logo.jpg',
+    url: 'https://suzukiseminar.vercel.app',
   },
   icons: {
-    icon: '',
+    icon: '/images/icons/favicon.ico',
   },
 };
 
@@ -37,7 +32,6 @@ export default async function RootLayout({ children }: Props) {
   return (
     <html lang="ja">
       <head>
-        {/* GoogleAnalytics */}
         <Script async strategy="lazyOnload" src={process.env.GOOGLE_ANALYTICS_ID} />
         <Script
           id="google-analytics"
@@ -53,35 +47,26 @@ export default async function RootLayout({ children }: Props) {
           `,
           }}
         />
-        {/* 共通 OK */}
         <meta name="format-detection" content="email=no,telephone=no,address=no" />
-        {/* 共通 ファビコン OK */}
-        <link rel="icon" href="" sizes="16x16" type="image/png" />
-        <link rel="icon" href="" sizes="32x32" type="image/png" />
-        <link rel="icon" href="" sizes="48x48" type="image/png" />
-        {/* 共通 Appleアイコン OK */}
-        <link rel="apple-touch-icon" href="" />
-
-        {/* 共通 Windows用アイコン OK */}
-        <meta name="msapplication-TileImage" content="" />
-        <meta name="msapplication-TileColor" content="#E0CBBA" />
-        {/* 共通 Twitter用 OK */}
-        <meta property="og:type" content="blog" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png" />
+        <meta name="msapplication-TileImage" content="/images/icons/mstile-150x150.png" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="" />
-        <meta name="twitter:site" content="" />
-        <meta property="og:site_name" content="" />
+        {/* <meta name="twitter:creator" content="" />
+        <meta name="twitter:site" content="" /> */}
+        <meta
+          property="og:site_name"
+          content="鈴木ゼミ｜東洋大学経営学部マーケティング学科鈴木ゼミナール"
+        />
         <meta property="og:locale" content="ja_JP" />
-
-        {/* Search Console */}
-        {/* <meta name="google-site-verification" content={process.env.SEARCH_CONSOLE_ID} /> */}
       </head>
       <body>
         <Header />
-        {/* <Nav tags={tags.contents} /> */}
         <main className={styles.main}>{children}</main>
         <Footer />
-        {/* <Script async strategy="afterInteractive" src="//www.instagram.com/embed.js" /> */}
       </body>
     </html>
   );
