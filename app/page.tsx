fabf1f0d9ab67c06abc54697cc2eae39e475f021
Arata <aratadayoon@yahoo.co.jp>
2024-10-04 11:43:00 +0900

    fix: TopPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,128 @@
+'use client';
+
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 export const revalidate = 60;
 
-export default async function Page() {
+export default function Page() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    arrows: true,
+    pauseOnHover: false,
+  };
+
+  const slides = [
+    {
+      mobile: '/images/top/mobile/1.jpeg',
+      tablet: '/images/top/tablet/1.jpeg',
+      desktop: '/images/top/pc/1.jpeg',
+      text: '9月の夏合宿ではBBQを楽しみました！',
+    },
+    {
+      mobile: '/images/top/mobile/1.jpeg',
+      tablet: '/images/top/tablet/1.jpeg',
+      desktop: '/images/top/pc/1.jpeg',
+      text: '9月の夏合宿ではBBQを楽しみました！',
+    },
+    {
+      mobile: '/images/top/mobile/1.jpeg',
+      tablet: '/images/top/tablet/1.jpeg',
+      desktop: '/images/top/pc/1.jpeg',
+      text: '9月の夏合宿ではBBQを楽しみました！',
+    },
+  ];
+
+  const products = [
+    {
+      id: 1,
+      name: '活動内容',
+      href: '/activity',
+      imageSrc: '/images/top/2A551869-3AA7-41BF-B32D-ECEF06ECC2C8_1_201_a.jpeg',
+      imageAlt: 'Hand stitched, orange leather long wallet.',
+    },
+    {
+      id: 1,
+      name: '組織',
+      href: '/member',
+      imageSrc: '/images/top/2A551869-3AA7-41BF-B32D-ECEF06ECC2C8_1_201_a.jpeg',
+      imageAlt: 'Hand stitched, orange leather long wallet.',
+    },
+    {
+      id: 1,
+      name: '選考',
+      href: '/selection',
+      imageSrc: '/images/top/2A551869-3AA7-41BF-B32D-ECEF06ECC2C8_1_201_a.jpeg',
+      imageAlt: 'Hand stitched, orange leather long wallet.',
+    },
+    {
+      id: 1,
+      name: 'お問い合わせ',
+      href: '/contact',
+      imageSrc: '/images/top/2A551869-3AA7-41BF-B32D-ECEF06ECC2C8_1_201_a.jpeg',
+      imageAlt: 'Hand stitched, orange leather long wallet.',
+    },
+  ];
+
   return (
-    <div style={{ marginTop: '120px', marginBottom: '120px' }}>
-      <div className="overflow-hidden">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8 main_side">
-          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
-            <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
-              <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                鈴木 寛ゼミナール
-              </h2>
-              <div className="mt-10 text-center text-4xl">消費者行動 × 企業戦略</div>
-              <p className="mt-10 text-xl leading-8 text-gray-600">
-                鈴木ゼミナールでは、日ごろの購買行動について解明を試み、『消費者の視点』と、消費者を引きつけて商品を買ってもらうための戦略を策定する『企業の視点』の双方を理解することに努めています。経営学部内のゼミや他大学との交流を通じて、消費者行動学を追求します。
-              </p>
-            </div>
-            {/* 済み */}
-            <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
-              <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+    <>
+      <div style={{ marginTop: '80px' }}>
+        <Slider {...settings}>
+          {slides.map((slide, index) => (
+            <div key={index} className="relative">
+              <picture>
+                <source media="(max-width: 600px)" srcSet={slide.mobile} />
+                <source media="(min-width: 601px) and (max-width: 1024px)" srcSet={slide.tablet} />
+                <source media="(min-width: 1025px)" srcSet={slide.desktop} />
                 <img
+                  src={slide.desktop}
                   alt=""
-                  src="images/top/81D4A81A-8979-4160-9C6B-E01A353169F5.jpeg"
-                  className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                  className="w-full max-w-none flex-none bg-gray-50 object-cover"
                 />
+              </picture>
+              <div className="absolute bottom-8 right-8 SlideText font-bold z-100 text-white">
+                <p>{slide.text}</p>
               </div>
-              {/* 済み */}
-              <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
-                <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
-                  <img
-                    alt=""
-                    src="images/top/2A551869-3AA7-41BF-B32D-ECEF06ECC2C8_1_201_a.jpeg"
-                    className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
-                  />
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      <div style={{ padding: '0 24px 24px 24px' }}>
+        <div className="relative bg-white">
+          <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl main_side">
+            <div className="TopSubTitle mt-20 font-bold text-center">消費者行動 × 企業戦略</div>
+            <div className="TopSubTitleText m-auto mt-5">
+              鈴木ゼミナールでは、日ごろの購買行動について解明を試み、『消費者の視点』と、消費者を引きつけて商品を買ってもらうための戦略を策定する『企業の視点』の双方を理解することに努めています。経営学部内のゼミや他大学との交流を通じて、消費者行動学を追求します。
+            </div>
+            <div className="mt-20 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
+              {products.map((product) => (
+                <div key={product.id} className="group relative">
+                  <div className="h-full w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75">
+                    <a href={product.href}>
+                      <img
+                        alt={product.imageAlt}
+                        src={product.imageSrc}
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </a>
+                    <div className="ActivityText absolute top-5 left-5 text-2xl font-bold">
+                      <p>{product.name}</p>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
-                  <img
-                    alt=""
-                    src="images/top/648085C8-898B-4E50-B62C-93DA654B235E.jpeg"
-                    className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
-                  />
-                </div>
-                {/* 済み */}
-                <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
-                  <img
-                    alt=""
-                    src="images/top/131D523B-BD66-4FE2-9E40-39E19F2BA8D3.jpeg"
-                    className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
-                  />
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
