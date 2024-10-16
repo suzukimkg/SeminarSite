84790369b025298259cc3dfce32543a32fb155d9
Arata <aratadayoon@yahoo.co.jp>
2024-10-17 08:07:14 +0900

    feat: 選考ページ

--- app/selection/page.tsx
+++ app/selection/page.tsx
@@ -4,11 +4,13 @@ export const revalidate = 60;
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
-  title: '選考 - 鈴木ゼミ｜東洋大学経営学部マーケティング学科鈴木ゼミナール',
-  description: '東洋大学経営学部マーケティング学科鈴木ゼミナールの選考についてを紹介しています。',
+  title: '選考フロー - 鈴木ゼミ｜東洋大学経営学部マーケティング学科鈴木ゼミナール',
+  description:
+    '東洋大学経営学部マーケティング学科鈴木ゼミナールの選考フローについてを紹介しています。',
   openGraph: {
-    title: '選考 - 鈴木ゼミ｜東洋大学経営学部マーケティング学科鈴木ゼミナール',
-    description: '東洋大学経営学部マーケティング学科鈴木ゼミナールの選考についてを紹介しています。',
+    title: '選考フロー - 鈴木ゼミ｜東洋大学経営学部マーケティング学科鈴木ゼミナール',
+    description:
+      '東洋大学経営学部マーケティング学科鈴木ゼミナールの選考フローについてを紹介しています。',
     images: '/images/icons/1200logo.jpg',
     url: 'https://suzuki-seminar.com/selection',
   },
