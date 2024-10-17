3a74b2545ec687bf1d57764e33ec8cc310e36fab
Arata <aratadayoon@yahoo.co.jp>
2024-10-18 02:10:41 +0900

    chore:

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = [...config.externals, 'undici'];
+    }
+    return config;
+  },
+};
