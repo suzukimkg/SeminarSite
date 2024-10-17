9b29572201b8d7b95de61e2b2e3c4b122465b398
Arata <aratadayoon@yahoo.co.jp>
2024-10-18 02:48:06 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-    });
-    return config;
+const nextConfig = {
+  experimental: {
+    externalDir: true,
   },
 };
+
+module.exports = nextConfig;
