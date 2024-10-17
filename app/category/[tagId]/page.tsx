1ffce88e111080e3919e3dbec35d7581c706d6cf
Arata <aratadayoon@yahoo.co.jp>
2024-10-18 02:00:43 +0900

    update: blog

--- app/category/[tagId]/page.tsx
+++ app/category/[tagId]/page.tsx
@@ -1,26 +1,30 @@
 import { getList } from '@/libs/microcms';
-import ArticleList from '@/components/ArticleList';
+import { getTag } from '@/libs/microcms';
+import { LIMIT } from '@/constants';
 import Pagination from '@/components/Pagination';
-import { HomeIcon, ChevronRightIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+import ArticleList from '@/components/ArticleList';
+import { HomeIcon, ChevronRightIcon, FolderOpenIcon } from '@heroicons/react/24/solid';
 
 type Props = {
-  searchParams: {
-    q?: string;
+  params: {
+    tagId: string;
   };
 };
 
 export const revalidate = 60;
 
-export default async function Page({ searchParams }: Props) {
+export default async function Page({ params }: Props) {
+  const tag = await getTag(params.tagId);
+  const { tagId } = params;
   const data = await getList({
-    q: searchParams.q,
+    limit: LIMIT,
+    filters: `tags[contains]${tagId}`,
   });
-
   return (
     <>
       <div
         className="mx-auto max-w-7xl px-6 text-center lg:px-8 main_side"
-        style={{ marginTop: '80px' }}
+        style={{ marginTop: '50px' }}
       >
         <h1 className="categoryTitle text-3xl font-bold pt-5 max-w-[85rem] mx-auto pb-2">
           <nav className="flex" aria-label="Breadcrumb">
@@ -36,19 +40,24 @@ export default async function Page({ searchParams }: Props) {
                     className="h-4 w-4 flex-shrink-0 text-gray-400"
                     aria-hidden="true"
                   />
-                  <div className="ml-4 text-sm font-medium text-gray-500">キーワードで探す</div>
+                  <a
+                    href={`${tag.id}`}
+                    className="ml-4 text-sm font-medium text-gray-500 hover:text-blue-500"
+                  >
+                    {tag.name}
+                  </a>
                 </div>
               </li>
             </ol>
           </nav>
           <div className="flex items-center pb-2 pt-2 mt-5">
-            <MagnifyingGlassIcon className="h-8 w-8 mr-2" aria-hidden="true" />
-            <div>キーワードで探す</div>
+            <FolderOpenIcon className="h-8 w-8 mr-2" aria-hidden="true" />
+            <div>{tag.name}</div>
           </div>
         </h1>
       </div>
       <ArticleList articles={data.contents} />
-      <Pagination totalCount={data.totalCount} basePath="/search" q={searchParams.q} />
+      <Pagination totalCount={data.totalCount} basePath={`/tags/${tagId}`} />
     </>
   );
 }
