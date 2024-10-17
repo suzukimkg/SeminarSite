cf0da82051f212c6bcd7752f989d4438756ea595
Arata <aratadayoon@yahoo.co.jp>
2024-10-17 20:23:40 +0900

    update: member

--- components/Member/index.tsx
+++ components/Member/index.tsx
@@ -15,55 +15,43 @@ export default function Member() {
   ];
   const people = [
     {
-      name: '城下 くうが',
-      role: 'ゼミ長',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      name: 'ゼミ長',
+      imageUrl: '/images/member/roles/6.png',
       // xUrl: '#',
       description:
-        '秋の風が頬に触れると、木々の葉が赤や黄に染まり、道端に落ちた葉のカサカサとした音が響く。澄んだ空の下を歩いていると、遠くから小鳥のさえずりが聞こえてきて、心が和む瞬間だ。',
+        'ゼミ長の主な仕事は、ゼミの運営を円滑に進めることです。具体的には、日程調整やメンバー間の連絡、進行状況の確認、イベント時の挨拶などを行います。また、ゼミの目標を共有し、良い雰囲気を作りながらメンバー同士の仲を深めることに努めています。',
     },
     {
-      name: '小栗 ともたか',
-      role: '副ゼミ長',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      name: '副ゼミ長',
+      imageUrl: '/images/member/roles/3.png',
       // xUrl: '#',
       description:
         '副ゼミ長の仕事は、ゼミ長のサポートとしてゼミの運営を円滑に進める役割です。ゼミ活動の調整、資料準備、連絡事項の共有、ゼミ長不在時の代理業務、ゼミ生間の意見調整などを行い、ゼミ全体のサポートを担います。',
     },
     {
-      name: '近藤 れおん',
-      role: 'イベント',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      name: 'イベント',
+      imageUrl: '/images/member/roles/4.png',
       // xUrl: '#',
       description:
         'イベント係では、ディズニー視察、学期末お疲れ様会、夏合宿それぞれの企画、運営を行います。学年を越えた交流や、組織全体のコミュニケーションを促す大事な役割です。鈴木ゼミの風通しの良さを作ってくれているのは間違いなくイベント係のおかげ！',
     },
     {
-      name: '服部 みさき',
-      role: '広報',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      name: '広報',
+      imageUrl: '/images/member/roles/2.png',
       // xUrl: '#',
       description:
         '広報係はゼミの魅力、活動、成果を多くの人に伝えています。主にSNSの情報発信を通して新しいメンバーを募集したりゼミ内での活動報告を行なっております。',
     },
     {
-      name: '高野 あらた',
-      role: '運営',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      name: '運営',
+      imageUrl: '/images/member/roles/5.png',
       // xUrl: '#',
       description:
         '運営係では、ディベート大会や夏合宿の設営を行います。ゼミの発表、各イベントが円滑に進むように班を越えた積極的なコミュニケーションをゼミ全体に促します。鈴木ゼミを陰から支えるまさかに"縁の下の力持ち"の役割を果たしています！',
     },
     {
-      name: '大谷 はるき',
-      role: '渉外',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      name: '渉外',
+      imageUrl: '/images/member/roles/1.png',
       // xUrl: '#',
       description:
         '渉外係では、主にディベート大会の企画、運営を行います。年に２回開催されるディベート大会をスムーズに進行できるよう、事前に他大学と打ち合わせをします。ディベートを通じて、企業の経営戦略やビジネスモデルの有効性を学ぶ機会を渉外係が中心になって作ります。',
@@ -153,7 +141,7 @@ export default function Member() {
           <h1 className="text-3xl font-bold">
             <div className="flex items-center pb-2 pt-2 mt-10">
               <CheckBadgeIcon className="h-8 w-8 mr-2" aria-hidden="true" />
-              <div>メンバー</div>
+              <div>役割</div>
             </div>
           </h1>
 
@@ -171,8 +159,9 @@ export default function Member() {
                 <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-500">
                   {person.name}
                 </h3>
-                <p className="text-sm leading-6 text-green-500">{person.role}</p>
-                <p className="text-sm leading-6 text-left text-gray-400">{person.description}</p>
+                <p className="mt-2 text-sm leading-6 text-left text-gray-400">
+                  {person.description}
+                </p>
               </li>
             ))}
           </ul>
