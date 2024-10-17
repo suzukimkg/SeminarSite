9adc46319a17be925a8d838dae3fbaf11e3a43ad
Arata <aratadayoon@yahoo.co.jp>
2024-10-17 23:32:50 +0900

    add: blog

---
+++ app/tags/[tagId]/page.tsx
@@ -0,0 +1,26 @@
+import { getList } from '@/libs/microcms';
+import { LIMIT } from '@/constants';
+import Pagination from '@/components/Pagination';
+import ArticleList from '@/components/ArticleList';
+
+type Props = {
+  params: {
+    tagId: string;
+  };
+};
+
+export const revalidate = 60;
+
+export default async function Page({ params }: Props) {
+  const { tagId } = params;
+  const data = await getList({
+    limit: LIMIT,
+    filters: `tags[contains]${tagId}`,
+  });
+  return (
+    <>
+      <ArticleList articles={data.contents} />
+      <Pagination totalCount={data.totalCount} basePath={`/tags/${tagId}`} />
+    </>
+  );
+}
