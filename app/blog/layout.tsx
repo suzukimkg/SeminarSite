ce75aec2af8f27d07e0d1227c4a59346c4fdd761
Arata <aratadayoon@yahoo.co.jp>
2024-10-18 08:17:19 +0900

    add: blog meta

---
+++ app/blog/layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Metadata } from 'next';
+
+type Props = {
+  children: React.ReactNode;
+  params: {
+    tagId: string;
+  };
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `ブログ - 東洋大学経営学部マーケティング学科鈴木ゼミナール`,
+    description: `ブログを公開しているページです。`,
+    openGraph: {
+      title: `ブログ - 東洋大学経営学部マーケティング学科鈴木ゼミナール`,
+      description: `ブログを公開しているページです。`,
+      images: `/images/icons/1200logo.jpg`,
+      url: `https://suzuki-seminar.com/blog`,
+    },
+  };
+}
+
+export default async function TagsLayout({ children }: Props) {
+  return (
+    <>
+      <>{children}</>
+    </>
+  );
+}
