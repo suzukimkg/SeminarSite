0184330d45363ae07c14a3c160bc7177e6527954
Arata <aratadayoon@yahoo.co.jp>
2024-10-18 23:49:03 +0900

    add: share

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -22,6 +22,23 @@ import {
 } from '@heroicons/react/24/solid';
 import Sidebar from '../Sidebar';
 
+import {
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  LineShareButton,
+  LineIcon,
+  HatenaShareButton,
+  HatenaIcon,
+  PinterestShareButton,
+  PinterestIcon,
+  RedditShareButton,
+  RedditIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+} from 'react-share';
+
 type Props = {
   data: Article;
 };
@@ -375,6 +392,80 @@ export default function ArticleComponent({ data }: Props) {
                         </div>
                       ))}
                     </div>
+                  </div>
+                  <div style={{ paddingTop: '12px' }}>
+                    <h1
+                      className={`${styles.profile} text-2xl font-semibold flex justify-center mb-5`}
+                    >
+                      <HandThumbUpIcon className="h-8 w-8 mr-2" aria-hidden="true" />
+                      シェアする
+                    </h1>
+                  </div>
+                  <div className="flex justify-center">
+                    <TwitterShareButton
+                      aria-label="シェアボタン"
+                      url={`https://suzuki-seminar.com/articles/${data.id}`}
+                      title={data.title}
+                      className="m-1"
+                    >
+                      <TwitterIcon size={40} round={true} />
+                    </TwitterShareButton>
+
+                    <FacebookShareButton
+                      aria-label="シェアボタン"
+                      url={`https://suzuki-seminar.com/articles/${data.id}`}
+                      title={data.title}
+                      className="m-1"
+                    >
+                      <FacebookIcon size={40} round={true} />
+                    </FacebookShareButton>
+
+                    <LineShareButton
+                      aria-label="シェアボタン"
+                      url={`https://suzuki-seminar.com/articles/${data.id}`}
+                      title={data.title}
+                      className="m-1"
+                    >
+                      <LineIcon size={40} round={true} />
+                    </LineShareButton>
+
+                    <HatenaShareButton
+                      aria-label="シェアボタン"
+                      url={`https://suzuki-seminar.com/articles/${data.id}`}
+                      title={data.title}
+                      className="m-1"
+                    >
+                      <HatenaIcon size={40} round={true} />
+                    </HatenaShareButton>
+
+                    <PinterestShareButton
+                      aria-label="シェアボタン"
+                      url={`https://suzuki-seminar.com/articles/${data.id}`}
+                      media={data.thumbnail?.url || ''}
+                      description={data.title}
+                      className="m-1"
+                    >
+                      <PinterestIcon size={40} round={true} />
+                    </PinterestShareButton>
+
+                    <RedditShareButton
+                      aria-label="シェアボタン"
+                      url={`https://suzuki-seminar.com/articles/${data.id}`}
+                      title={data.title}
+                      className="m-1"
+                    >
+                      <RedditIcon size={40} round={true} />
+                    </RedditShareButton>
+
+                    <LinkedinShareButton
+                      aria-label="シェアボタン"
+                      url={`https://suzuki-seminar.com/articles/${data.id}`}
+                      title={data.title}
+                      summary={data.description}
+                      className="m-1"
+                    >
+                      <LinkedinIcon size={40} round={true} />
+                    </LinkedinShareButton>
                   </div>
                 </main>
               </div>
