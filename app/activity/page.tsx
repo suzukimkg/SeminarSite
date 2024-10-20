eddab25d783ee2be8396ddc8997ecda741a87dda
Arata <aratadayoon@yahoo.co.jp>
2024-10-20 22:10:28 +0900

    fix: pwa header

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,4 +1,5 @@
 import Activity from '@/components/Fixed/Activity';
+import { isAndroid, isIOS } from 'react-device-detect';
 
 export const revalidate = 60;
 
@@ -15,9 +16,14 @@ export const metadata = {
 };
 
 export default async function Page() {
+  const isPWA =
+    typeof window !== 'undefined' && window.matchMedia('(display-mode: standalone)').matches;
   return (
     <>
-      <div style={{ padding: '24px' }}>
+      <div
+        style={{ padding: '24px' }}
+        className={`${(isAndroid || isIOS) && !isPWA ? 'margin-top-35' : 'margin-top-0'}`}
+      >
         <Activity />
       </div>
     </>
