8b55c8a342420eeef1e834945e104ece172e5d7d
Arata <aratadayoon@yahoo.co.jp>
2024-10-20 22:15:31 +0900

    Revert "fix: pwa header"
    This reverts commit 81e867c8b3bef2667defb216b38101c5408bc067.

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,5 +1,4 @@
 import Activity from '@/components/Fixed/Activity';
-import { isAndroid, isIOS } from 'react-device-detect';
 
 export const revalidate = 60;
 
@@ -16,14 +15,9 @@ export const metadata = {
 };
 
 export default async function Page() {
-  const isPWA =
-    typeof window !== 'undefined' && window.matchMedia('(display-mode: standalone)').matches;
   return (
     <>
-      <div
-        style={{ padding: '24px' }}
-        className={`${(isAndroid || isIOS) && !isPWA ? 'margin-top-35' : 'margin-top-0'}`}
-      >
+      <div style={{ padding: '24px' }}>
         <Activity />
       </div>
     </>
