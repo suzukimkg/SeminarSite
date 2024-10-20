e146215fb029164ee09fbd381f7e96f425c80e96
Arata <aratadayoon@yahoo.co.jp>
2024-10-20 10:51:51 +0900

    fix: pwa

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,10 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { useState, Fragment } from 'react';
-import { useA2HS } from '@/hooks/A2hs';
-import { isAndroid, isIOS } from 'react-device-detect';
-import { Dialog, Transition } from '@headlessui/react';
-import { XMarkIcon } from '@heroicons/react/24/solid';
-import { ArrowUpOnSquareIcon } from '@heroicons/react/24/outline';
 
 export const revalidate = 60;
 
 export default function Page() {
-  const [open, setOpen] = useState(false);
-
   const settings = {
     dots: true,
     infinite: true,
@@ -77,37 +69,13 @@ export default function Page() {
     },
   ];
 
-  const [, promptToInstall] = useA2HS({
-    onAccepted: () => {
-      console.log('ホーム画面に追加が受け入れられました');
-    },
-    onDismissed: () => {
-      console.log('ホーム画面に追加が拒否されました');
-    },
-  });
-
   return (
     <>
-      <div style={{ marginTop: '80px' }}>
+      <div style={{ marginTop: '120px' }}>
         <div className="TopBanner flex justify-center gap-x-1 bg-green-700 px-6 py-2.5 sm:px-3.5">
           <p className="text:sm sm:text-2xl font-bold leading-6 text-white">
             ようこそ、東洋大学 鈴木ゼミナールへ
           </p>
-          {isAndroid || !isIOS ? (
-            <button
-              onClick={promptToInstall}
-              className="flex-none rounded-full bg-white px-3.5 py-1 text-base font-semibold text-black shadow-sm hover:text-green-500"
-            >
-              アプリを追加する <span aria-hidden="true">&rarr;</span>
-            </button>
-          ) : isIOS ? (
-            <button
-              onClick={() => setOpen(true)}
-              className="flex-none rounded-full bg-white px-3.5 py-1 text-base font-semibold text-black shadow-sm hover:text-green-500"
-            >
-              アプリを追加する <span aria-hidden="true">&rarr;</span>
-            </button>
-          ) : null}
         </div>
         <Slider {...settings}>
           {slides.map((slide, index) => (
@@ -158,72 +126,6 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={setOpen}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                  <div className="absolute right-0 top-0 pr-4 pt-4 sm:block">
-                    <button
-                      type="button"
-                      onClick={() => setOpen(false)}
-                      className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-                    >
-                      <span className="sr-only">Close</span>
-                      <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-                    </button>
-                  </div>
-                  <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <ArrowUpOnSquareIcon aria-hidden="true" className="h-6 w-6 text-green-700" />
-                    </div>
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        最新情報を逃すな！
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          プッシュ通知を受け取ることができます。
-                          <br />
-                          <br />
-                          <div className="text-left">
-                            画面下部の 「シェアアイコン」
-                            をタップして「ホーム画面に追加」を選択してください！
-                          </div>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition.Root>
     </>
   );
 }
